3091111833dcc08679bae40f3c925366b5497755
Kelly Eldritch <kellyjanderson@users.noreply.github.com>
2026-09-20 12:22:42 -0700

    Export round coordinator API

--- src/dynamic_story_scaffold/__init__.py
+++ src/dynamic_story_scaffold/__init__.py
@@ -1,11 +1,15 @@
 """Dynamic Story Scaffold: bounded stochastic scene simulation."""
 
+from .coordinator import CoordinatorError, RoundCoordinator, RoundInProgressError
 from .loader import load_scene, parse_scene
 from .schema import SceneDefinition
 from .simulation import Simulation
 from .state import WorldState
 
 __all__ = [
+    "CoordinatorError",
+    "RoundCoordinator",
+    "RoundInProgressError",
     "SceneDefinition",
     "Simulation",
     "WorldState",
